0202e55960f4a6d3592f1e72afec2e77f5b11649
fgabel <kpazrael2@gmail.com>
2019-06-05 16:19:26 +0200

    DataGenerator finished, optimisations include vectorizing batch loading with numpy

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -7,15 +7,21 @@ import numpy as np
 # Batch size 1 is fine for VGG
 BATCH_SIZE = 1
 
-def data_generator(dataset_type, data_name_list, binary_labels=False):
-    while True:
 
-        random.shuffle(data_name_list)
+class DataGenerator:
+    """
 
-        X = []
-        Y = []
-        for data_name in data_name_list:
+    """
+    def __init__(self, config):
+        self.config = config
+        # load data here
+        get_training_data_list(mode, cnofig.datapath #todo)
+        self.input = np.ones((500, 784))
+        self.y = np.ones((500, 10))
 
+    def next_batch(self, batch_size):
+        batch = np.random.choice(data_name_list, batch_size)
+        for data_name in batch:
             image_data_volume = get_image_data(dataset_type, data_name)
 
             if binary_labels:
@@ -26,29 +32,7 @@ def data_generator(dataset_type, data_name_list, binary_labels=False):
             X.append(image_data_volume)
             Y.append(label_data)
 
-            if len(X) == BATCH_SIZE:
-                yield np.array(X), np.array(Y)
-                X = []
-                Y = []
-
-# TODO: turn the above function into an appropriate DataGenerator class!
-
-
-class DataGenerator:
-
-    """
-
-    """
-    def __init__(self, config):
-        self.config = config
-        # load data here
-        get_training_data_list
-        self.input = np.ones((500, 784))
-        self.y = np.ones((500, 10))
-
-    def next_batch(self, batch_size):
-        idx = np.random.choice(500, batch_size)
-        yield self.input[idx], self.y[idx]
+            yield np.array(X), np.array(Y)
 
 def get_training_data_list(training_folder_images_path, IMAGES_SUFFIX):
     """
