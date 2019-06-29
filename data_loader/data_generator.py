ce857ab1e9d6a4bf2b1957e0092c0f98e41e1eed
fgabel <kpazrael2@gmail.com>
2019-06-29 20:23:07 +0200

    odified data loader to load validation data, don't forget to change folder in train_config

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -3,35 +3,40 @@ import os
 import skimage.io
 import skimage.transform
 import numpy as np
-from utils.data_utils import get_image_data, get_binary_label_data, get_label_data, preprocess_image_data, load_image, load_labels
+from utils.data_utils import get_image_data, get_binary_label_data, get_label_data, preprocess_image_data, load_image, \
+    load_labels
 from utils.config import get_config_from_json
 
 # Batch size 1 is fine for VGG
 BATCH_SIZE = 1
 
+
 class DataGenerator:
     """
     Class for data generation in batches(both train and validation)
     """
-    def __init__(self, dataconfig, mode = "training"):
+
+    def __init__(self, dataconfig):
         self.config = dataconfig
         # print(self.config.dataset_folders[mode])
-        self.mode = mode
+
         # load data here
-        self.data_name_list = get_training_data_list(self.config.dataset_folders[mode]+'/images', self.config.images_suffix)
+
         # print(self.data_name_list)
         # Note: we need binary label data during training!
         self.binary_labels = 1
 
-    def next_batch(self, batch_size):
-        X = [] # care that this does not cause unintended consequences
+    def next_batch(self, batch_size, mode):
+        X = []  # care that this does not cause unintended consequences
         Y = []
+        self.mode = mode
+        self.data_name_list = get_training_data_list(self.config.dataset_folders[mode] + '/images',
+                                                     self.config.images_suffix)
         batch = np.random.choice(self.data_name_list, batch_size)
-
         for data_name in batch:
             image_data_volume = get_image_data(self.mode, data_name, self.config.dataset_folders,
                                                self.config.images_directory_name, self.config.images_suffix,
-                                               data_mean = self.config.data_mean, data_std = self.config.data_std)
+                                               data_mean=self.config.data_mean, data_std=self.config.data_std)
 
             if self.binary_labels:
                 label_data = get_binary_label_data(self.mode, data_name, self.config.dataset_folders,
@@ -48,6 +53,7 @@ class DataGenerator:
         # yield np.array(X), np.array(Y)
         return np.array(X), np.array(Y)
 
+
 def get_training_data_list(training_folder_images_path, images_suffix):
     """
     Return a list of training images given their suffix and a path
@@ -58,7 +64,6 @@ def get_training_data_list(training_folder_images_path, images_suffix):
     training_data_name_list = []
 
     for training_image_path in os.listdir(training_folder_images_path):
-
         training_data_name = training_image_path[0:len(training_image_path) - len(images_suffix)]
         # print(training_data_name)
         training_data_name_list.append(training_data_name)
