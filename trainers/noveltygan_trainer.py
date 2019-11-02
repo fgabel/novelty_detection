bf9a1f5fc735366d96877aa71fc355f62bcc0cc6
Felix Feldmann <felix@bnbit.de>
2019-11-02 21:16:12 +0100

    changes to training schedule / move everything to master

--- trainers/noveltygan_trainer.py
+++ trainers/noveltygan_trainer.py
@@ -134,16 +134,17 @@ class NoveltyGANTrainer():
         train_loss_mixed = []
         train_loss_gan_from_dis = []
         train_loss_gan_from_gen = []
+        train_loss_generator = []
         metrics_dict = dict()
         for _ in loop:
             #log_gan, train_loss_discriminator_true, train_loss_discriminator_fake = self.train_step()
-            log_gan, train_loss_discriminator_mixed, train_loss_gan_from_dis_, train_loss_gan_from_gen_ = self.train_step()
+            log_gan, train_loss_discriminator_mixed, train_loss_gan_from_dis_, train_loss_gan_from_gen_, train_loss_generator_ = self.train_step()
             logs.append(log_gan)
             #print('i expected this to be accurary: ', train_loss_gan_from_dis_)
             train_loss_gan_from_dis.append(train_loss_gan_from_dis_)
             train_loss_gan_from_gen.append(train_loss_gan_from_gen_)
             train_loss_mixed.append(train_loss_discriminator_mixed)
-            
+            train_loss_generator.append(train_loss_generator_)
         logs_avg = np.mean(logs, axis=0)
         #metrics_dict['train loss discriminator on true data'] = np.mean(train_loss_dt)
         #metrics_dict['train loss discriminator on fake data'] = np.mean(train_loss_df)
@@ -151,6 +152,7 @@ class NoveltyGANTrainer():
         print("TRAIN: Disriminators (mixed data) loss:", np.mean(train_loss_mixed))
         metrics_dict["TRAIN: GAN from D"] = np.mean(train_loss_gan_from_dis)
         metrics_dict["TRAIN: GAN from G"] = np.mean(train_loss_gan_from_gen)
+        metrics_dict["TRAIN: GEN"] = np.mean(train_loss_generator)
         #self.gan_model.gan.save_weights(os.path.join("../experiments", self.config.exp_name, "checkpoint/my_model.h5"))
         #self.modelcheckpoint.on_epoch_end(id)
 
@@ -183,7 +185,7 @@ class NoveltyGANTrainer():
         def evaluation_loop():
             """This function evaluates both the discriminator and the generator after each epoch"""
             # Discriminator evaluation on fake data
-            print("[VALIDATION] D loss and accuracy on fake data: ")
+            print("[VAL] GAN evaluation: D loss and accuracy (input img_batch, output [label_batch, zeroes])")
             dis_fake = self.gan_model.gan.evaluate(
                 img_batch,
                 [label_batch, np.zeros((self.data['val'].batch_size, self.gan_model.pixelwise_h, self.gan_model.pixelwise_w))]
@@ -191,17 +193,26 @@ class NoveltyGANTrainer():
             """
                 See comment on self.gan_model.gan.metrics_names in train_step_gan down below
             """
-            metrics_dict["VALIDATION: D_fake_loss"] = dis_fake[2]
-            metrics_dict["VALIDATION: D_fake_acc"] = dis_fake[4]
+            metrics_dict["VAL: GAN evaluation: D loss (input img_batch, output [label_batch, zeroes])"] = dis_fake[2]
+            metrics_dict["VAL: GAN evaluation: D acc (input img_batch, output [label_batch, zeroes])"] = dis_fake[4]
 
             # Discriminator evaluation on real data
-            print("[VALIDATION] D Loss on real data: ")
+            print("VAL: D evaluation (input [label, img], output [ones]")
             dis_real = self.gan_model.discriminator.evaluate(
                 [label_batch, img_batch],
                 np.ones((self.data['val'].batch_size, self.gan_model.pixelwise_h, self.gan_model.pixelwise_w))
             )
-            metrics_dict["VALIDATION: D_real_loss"] = dis_real 
+            gen_eval = self.gan_model.generator.evaluate(img_batch, label_batch)
 
+            metrics_dict["VAL: D evaluation (input [label, img], output [ones]"] = dis_real 
+            # generator raw
+            metrics_dict["VAL: G evaluation (input [img], output [label]"] = gen_eval
+            #print("VAL: D evaluation (input [label, gen(img)], output [zeros]")
+            #dis_fake = self.gan_model.discriminator.evaluate(
+            #    [softmax_output_to_binary_labels(self.gan_model.gan.predict_on_batch(img_batch)), img_batch],
+            #    np.zeros((self.data['val'].batch_size, self.gan_model.pixelwise_h, self.gan_model.pixelwise_w))
+            #)
+            #metrics_dict["VAL: D evaluation (input [label, gen(img)], output [zeros]"] = dis_fake 
             print("___________________")
             #print("[VALIDATION] Accucary real data: ")
             #print(self.gan_model.discriminator.predict([label_batch, img_batch]))
@@ -237,7 +248,7 @@ class NoveltyGANTrainer():
         evaluation_loop()
 
         self.tensorboard.on_epoch_end(id, logs=named_logs(self.gan_model.gan, logs_avg, metrics_dict))
-        self.gan_model.save(id, self.config.exp_name)
+        #self.gan_model.save(id, self.config.exp_name)
 
         return 0
 
@@ -287,8 +298,13 @@ class NoveltyGANTrainer():
         discriminator_input = [labels_batch, img_batch]
 
         loss = self.gan_model.discriminator.train_on_batch(discriminator_input, discriminator_ground_truth)
+        #print("Train step dis logs below, i.e. dis_loss using data mode: "+train_mode)
+        #print(loss)
         return loss
-
+    def train_step_generator(self):
+        img_batch, labels_batch = self.data['train'].next_batch()
+        loss = self.gan_model.generator.train_on_batch(img_batch, labels_batch)
+        return loss
     def train_step_gan(self):
         """
         Perform a single training step for the GAN with frozen discriminator
@@ -302,7 +318,7 @@ class NoveltyGANTrainer():
         gan_supervision = np.ones((self.config.batch_size, self.gan_model.pixelwise_h, self.gan_model.pixelwise_w))
 
         logs = self.gan_model.gan.train_on_batch(img_batch, [label_batch, gan_supervision])
-        print("Train step gan logs below")
+        print("Train step gan logs below, i.e. combined loss, gen_loss, dis_loss, gen_acc, dis_acc")
         print(logs)
         """
             Note: self.gan_model.gan.metrics_names [
@@ -324,15 +340,12 @@ class NoveltyGANTrainer():
         return logs[0], logs[2], logs[1]
 
     def train_step(self):
-        # TODO: come up with training schedule
-        #train_loss_discriminator_true = self.train_step_discriminator(train_mode="true_data")
-        #train_loss_discriminator_false = self.train_step_discriminator(train_mode="fake_data")
 
-
-        for _ in range(3):
-            train_loss_discriminator_mixed = self.train_step_discriminator(train_mode="mixed")
+        for _ in range(2):
+            train_loss_discriminator = self.train_step_discriminator(train_mode="mixed")
+            train_loss_generator = self.train_step_generator()
        
         train_loss_gan, train_loss_gan_from_dis, train_loss_gan_from_gen = self.train_step_gan()
-
-        return train_loss_gan, train_loss_discriminator_mixed, train_loss_gan_from_dis, train_loss_gan_from_gen
+        
+        return train_loss_gan, train_loss_discriminator, train_loss_gan_from_dis, train_loss_gan_from_gen, train_loss_generator
         #return train_loss_gan, train_loss_discriminator_true, train_loss_discriminator_false
